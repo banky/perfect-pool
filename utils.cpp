a21205ed88f828885915954f7c0e0a076dd9b209
dudds4 <dudds4@gmail.com>
2015-07-10 23:12:42 -0400

    fmlfmlfmlf

--- utils.cpp
+++ utils.cpp
@@ -1,5 +1,9 @@
 #include <iostream>
 #include "shot.h"
+#include "Ball_Class.hpp"
+#include "position.h"
+#include "GameConstants.hpp"
+#include <sstream>
 
 using namespace std;
 
@@ -15,4 +19,55 @@ void output (ostream &out, Shot* shots, int length)
 	}
 }
 
+int getNextInt(istream& in) {
+	char c = 'l';
+	while(c!='='){
+		in >> c;
+	}
+	int n;
+	in >> n;
+	return n;
+}
+double getNextDouble(istream& in) {
+	char c = 'l';
+	while(c!='='){
+		in >> c;
+	}
+	double d;
+	in >> d;
+	return d;
+}
 
+Ball getBallFromInput(istream& in, int ballNum) {
+	char c = 'l';
+	while(c!='[') {
+		in >> c;
+	}
+	int x, y, z;
+	in >> x >> c >> y >> c >> z;
+	Ball b(x, y, z, ballNum);
+	
+}
+
+void extractFileInput(istream& in, GameConstants* gc, vector<Ball>* balls) {
+	GameConstants gc;
+	string s;
+	s = getNextNumber(in);
+	s = s.substr();
+	double alpha = getNextDouble(in);
+	double dt = getNextDouble(in);
+	double vel_thresh = getNextDouble(in);
+	
+	int numShots = getNextInt(in);
+	int maxXVel = getNextInt(in);
+	int maxYVel = getNextInt(in);
+	
+	int width = getNextInt(in);
+	int length = getNextInt(in);
+	int b_radius = getNextInt(in);
+	int pocket_radius = getNextInt(in);
+	int pocket_offset = getNextInt(in);
+	
+	balls->push_to_back(getBallFromInput(in));
+	
+}

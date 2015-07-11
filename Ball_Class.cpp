0ac556b5a0cee627b80d11d34259fe053e06874f
bankya <bankiadebajo@hotmail.com>
2015-07-10 21:50:47 -0400

    Add collision functions

--- Ball_Class.cpp
+++ Ball_Class.cpp
@@ -1,28 +1,61 @@
 #include "Ball_Class.hpp"
+#include <cmath>
 
-	int ball::get_x_speed ()
-	{
-		return x_speed;
+//TODO: I'm not too sure how to get the mass value
+int mass;
+
+//Our time constant
+double dt = 0.001;
+int tableLength;
+int tableWidth;
+
+int ball::get_x_speed () {
+    return x_speed;
+}
+int ball::get_y_speed () {
+    return y_speed;
+}
+int ball::get_value () {
+	return value;
+}
+int ball::get_postion () {
+	return true; //should be x;
+}
+int ball::get_id () {
+	return id;
+}
+ball::ball (int x1, int y, int v,int i) {
+    x = position(x1,y);
+	value = v;
+	id =i;
+}
+ball::getRadius() {
+	return radius;
+}
+
+ball::ball_collision(ball b) {
+	int d = sqrt(pow((position.x - b.position.x),2) + pow(pow((position.y - b.position.y), 2)));
+	int nx = (b.position.x - position.x)/d;
+	int ny = (b.position.y - position.y)/d;
+    int p = (2 * (x_speed * nx + y_speed * ny - b.x_speed * nx - b.y_speed * ny))/(mass + mass);
+    x_speed = x_speed - p * mass * nx;
+    y_speed = y_speed - p * mass * ny;
+    b.x_speed = b.x_speed + p * mass * nx;
+    b.y_speed = b.y_speed + p * mass * ny;
+    
+    position.x += x_speed * dt;
+    position.y += y_speed * dt;
+    b.position.x += b.x_speed * dt;
+    b.position.y += b.y_speed * dt;
+}
+
+ball::table_collision() {
+    if (((position.x - radius) < 0) || ((position.x + radius) > tableWidth)) {
+		x_speed *= -1;
+		position.x += x_speed * dt;
 	}
-	int ball::get_y_speed ()
-	{
-		return y_speed;
+	if (((position.y - radius < 0) || ((position.y + radius) > length))) {
+		y_speed *= -1;
+		position.y += y_speed * dt;
 	}
-	int ball::get_value ()
-	{
-		return value;
-	}
-	int ball::get_postion ()
-	{
-		return true; //should be x;
-	}
-	int ball::get_id ()
-	{
-		return id; 
-	}
-	ball::ball (int x1, int y, int v,int i)
-	{
-		x = position(x1,y);
-		value = v; 
-		id =i; 
-	}
+}
--- Ball_Class.hpp
+++ Ball_Class.hpp
@@ -2,18 +2,19 @@
 #define BALLAR
 #include "Position.hpp"
 
-class ball 
+class Ball 
 {
 	public:	
-		ball (int x1, int y, int v,int i);
+		Ball (int x1, int y, int v,int i);
 		int get_x_speed ();
 		int get_y_speed ();
 		int get_value ();
 		int get_postion ();
 		int get_id ();
-		
+		int getRadius();
+		void ball_collision(ball b);		
 	private:
-		int x_speed,y_speed,value,id;
-		postion x;
+		int x_speed,y_speed,value,id,radius;
+		Postion position;
 };
 #endif

8569651ca1fe345d6adda4ccdcfb5261d81d67fc
Hella swag <awn@hotmail.com>
2015-07-10 20:47:45 -0400

    hit

---
+++ Ball_Class.hpp
@@ -0,0 +1,19 @@
+#ifndef BALLAR
+#define BALLAR
+#include "Position.hpp"
+
+class ball 
+{
+	public:	
+		ball (int x1, int y, int v,int i);
+		int get_x_speed ();
+		int get_y_speed ();
+		int get_value ();
+		int get_postion ();
+		int get_id ();
+		
+	private:
+		int x_speed,y_speed,value,id;
+		postion x;
+};
+#endif
